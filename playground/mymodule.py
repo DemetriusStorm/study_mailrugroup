da3a83aa9cc92bd91f31a7f46a30d01b75306d69
Demetrius Storm <47479409+DemetriusStorm@users.noreply.github.com>
2019-09-06 13:38:50 +0300

    Update

--- playground/mymodule.py
+++ playground/mymodule.py
@@ -1,3 +1,6 @@
-import mypackage
+# import mypackage.utils
+from mypackage.utils import multiply
 
-print(mypackage)
+if __name__ == '__main__':
+    print(multiply(2, 3))
+    # print(mypackage.utils.multiply(2, 3))
